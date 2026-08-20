fab2118bbd2f0834eb51f53f84507035a6a84293
Omar Alkhateeb <omaralkhateeb1w@gmail.com>
2026-08-20 18:10:08 +0300

    add_DbContext

---
+++ coop/CoopDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace coop
+{
+    public class CoopDbContext : DbContext
+    {
+       
+            public CoopDbContext(DbContextOptions<CoopDbContext> options)
+                : base(options)
+            {
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
