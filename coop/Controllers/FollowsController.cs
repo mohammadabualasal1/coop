9edc1fa7bb54ba113f4f4a7e1ea3acdd65a01a3b
Omar Alkhateeb <omaralkhateeb1w@gmail.com>
2026-08-25 17:38:14 +0300

    GetMyFollowedMerchants

--- coop/Controllers/FollowsController.cs
+++ coop/Controllers/FollowsController.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Security.Claims;
+using coop.Dtos.FollowsController;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace coop.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/follows")]
+    [Authorize(Roles = "Customer")]
     public class FollowsController : ControllerBase
     {
+        private CoopDbContext _dbcontext;
+
+        public FollowsController(CoopDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyFollowedMerchants()
+        {
+            var userId = GetCurrentUserId();
+
+            var follows = await _dbcontext.FollowedMerchants
+                .Where(f => f.CustomerUserId == userId)
+                .OrderByDescending(f => f.CreatedAt)
+                .Select(f => new FollowedMerchantResponseDto
+                {
+                    Id = f.Id,
+                    MerchantId = f.MerchantId,
+                    Name = f.Merchant.Name,
+                    LogoUrl = f.Merchant.LogoUrl,
+                    CreatedAt = f.CreatedAt
+                })
+                .ToListAsync();
+
+            return Ok(follows);
+        }
+
+        private Guid GetCurrentUserId() =>
+            Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
     }
 }
