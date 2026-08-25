eac8f82f834720d0332e7d86cf50b4452de5bc4d
Omar Alkhateeb <omaralkhateeb1w@gmail.com>
2026-08-25 15:35:11 +0300

    Add Dto

---
+++ coop/Dtos/MarketplaceDtos/NearbyOffersRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace coop.Dtos.MarketplaceController
+{
+    public class NearbyOffersRequestDto
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double RadiusKm { get; set; } = 10;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+    }
+}
