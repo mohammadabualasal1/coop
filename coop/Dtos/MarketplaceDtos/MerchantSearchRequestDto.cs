a05c503c5cab6834fea1ef189619210d5695f226
Omar Alkhateeb <omaralkhateeb1w@gmail.com>
2026-08-25 15:44:45 +0300

    MerchantSearchRequestDto

---
+++ coop/Dtos/MarketplaceDtos/MerchantSearchRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace coop.Dtos.MarketplaceController
+{
+    public class MerchantSearchRequestDto
+    {
+        public string? Search { get; set; }
+        public string? City { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+    }
+}
