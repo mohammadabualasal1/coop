d241d3088a64b0181ea9acbafef3b5475ad9a32e
Omar Alkhateeb <omaralkhateeb1w@gmail.com>
2026-08-25 16:04:14 +0300

    AddFavorite

--- coop/Controllers/FavoritesController.cs
+++ coop/Controllers/FavoritesController.cs
@@ -1,8 +1,9 @@
-﻿using System.Security.Claims;
-using coop.Dtos.FavoritesController;
+﻿using coop.Dtos.FavoritesController;
+using coop.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace coop.Controllers
 {
@@ -38,7 +39,44 @@ namespace coop.Controllers
 
             return Ok(favorites);
         }
+        [HttpPost("{offerId}")]
+        public async Task<IActionResult> AddFavorite(Guid offerId)
+        {
+            var userId = GetCurrentUserId();
 
+            var offer = await _dbcontext.Offers.FirstOrDefaultAsync(o => o.Id == offerId);
+            if (offer == null)
+                return NotFound("العرض غير موجود");
+
+            if (offer.EndAt < DateTime.UtcNow)
+                return BadRequest("لا يمكن إضافة عرض منتهٍ للمفضلة");
+
+            var alreadyFavorited = await _dbcontext.FavoriteOffers
+                .AnyAsync(f => f.CustomerUserId == userId && f.OfferId == offerId);
+
+            if (alreadyFavorited)
+                return BadRequest("العرض موجود بالفعل في المفضلة");
+
+            var favorite = new FavoriteOffer
+            {
+                Id = Guid.NewGuid(),
+                CustomerUserId = userId,
+                OfferId = offerId,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _dbcontext.FavoriteOffers.Add(favorite);
+            await _dbcontext.SaveChangesAsync();
+
+            return Ok(new FavoriteOfferResponse
+            {
+                Id = favorite.Id,
+                OfferId = favorite.OfferId,
+                Title = offer.Title,
+                DiscountedPrice = offer.DiscountedPrice,
+                CreatedAt = favorite.CreatedAt
+            });
+        }
 
 
 
