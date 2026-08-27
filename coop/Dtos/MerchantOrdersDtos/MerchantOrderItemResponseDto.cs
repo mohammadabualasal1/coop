cccce16915d2ca6a18a161828baabb676b10f34c
omar_alkhateeb <omaralkhateeb1w@gmail.com>
2026-08-27 23:55:40 +0300

    MerchantOrderItemResponseDto

---
+++ coop/Dtos/MerchantOrdersDtos/MerchantOrderItemResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace coop.Dtos.MerchantOrdersDtos
+{
+    public class MerchantOrderItemResponseDto
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal DiscountedUnitPrice { get; set; }
+        public decimal LineTotal { get; set; }
+    }
+}
