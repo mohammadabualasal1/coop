4915c9b984103f07617b7ab3fa56bb4483e1b37b
mohammadabualasal1 <moh2001ha@gmail.com>
2026-08-28 17:04:51 +0300

    AddDriverProfile

--- coop/Controllers/DriversController.cs
+++ coop/Controllers/DriversController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
+﻿using coop.Dtos.DriversController;
+using coop.Dtos.DriversDtos;
+using coop.Enums;
+using coop.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
-
 namespace coop.Controllers
 {
     [Route("api/[controller]")]
@@ -17,7 +20,46 @@ namespace coop.Controllers
             _dbcontext = dbcontext;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> AddDriverProfile([FromBody] CreateDriverProfileRequestDto dto)
+        {
+            var userId = GetCurrentUserId();
 
+            var exists = await _dbcontext.DriverProfiles.AnyAsync(d => d.UserId == userId);
+            if (exists)
+                return Conflict("لديك بروفايل سائق بالفعل");
+
+            if (dto.MaximumCapacity < 1)
+                return BadRequest("السعة القصوى يجب أن تكون 1 أو أكثر");
+
+            var newProfile = new DriverProfile
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId,
+                VehicleType = dto.VehicleType,
+                VehiclePlateNumber = dto.VehiclePlateNumber,
+                MaximumCapacity = dto.MaximumCapacity,
+                VerificationStatus = VerificationStatus.Pending,
+                IsAvailable = false,
+                CompletedDeliveries = 0,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _dbcontext.DriverProfiles.Add(newProfile);
+            await _dbcontext.SaveChangesAsync();
+
+            return StatusCode(201, new DriverProfileResponse
+            {
+                Id = newProfile.Id,
+                VehicleType = newProfile.VehicleType,
+                VehiclePlateNumber = newProfile.VehiclePlateNumber,
+                MaximumCapacity = newProfile.MaximumCapacity,
+                VerificationStatus = newProfile.VerificationStatus,
+                IsAvailable = newProfile.IsAvailable,
+                AverageRating = newProfile.AverageRating,
+                CompletedDeliveries = newProfile.CompletedDeliveries
+            });
+        }
 
 
 
