033e00753d5306d0b9ed4759fb2b2edada18616b
mohammadabualasal1 <moh2001ha@gmail.com>
2026-08-28 17:03:46 +0300

    DriversController

---
+++ coop/Controllers/DriversController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace coop.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Driver")]
+    [ApiController]
+    public class DriversController : ControllerBase
+    {
+        private CoopDbContext _dbcontext;
+
+        public DriversController(CoopDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+
+
+
+
+
+
+
+
+        private Guid GetCurrentUserId() =>
+        Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+    }
+}
---
+++ coop/Dtos/DriversDtos/CreateDriverProfileRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace coop.Dtos.DriversDtos
+{
+    public class CreateDriverProfileRequestDto
+    {
+        public string VehicleType { get; set; }
+        public string VehiclePlateNumber { get; set; }
+        public int MaximumCapacity { get; set; }
+    }
+}
