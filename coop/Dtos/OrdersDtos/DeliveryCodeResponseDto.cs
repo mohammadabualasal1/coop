81346141caa9adbf6fb95d80dfe3c92e35a31590
omar_alkhateeb <omaralkhateeb1w@gmail.com>
2026-08-28 20:07:09 +0300

    DeliveryCodeResponseDto

---
+++ coop/Dtos/OrdersDtos/DeliveryCodeResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace coop.Dtos.OrdersDtos
+{
+    public class DeliveryCodeResponseDto
+    {
+        public string Code { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
