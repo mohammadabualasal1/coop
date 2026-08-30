fa8d198b874596aa07428387ac206c4f97f85761
mohammadabualasal1 <moh2001ha@gmail.com>
2026-08-30 19:58:46 +0300

    GetMyNotifications

--- coop/Controllers/NotificationsController.cs
+++ coop/Controllers/NotificationsController.cs
@@ -1,6 +1,20 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using coop.Dtos.NotificationsDtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using coop.Dtos.MerchantBranchesController;
+using coop.Dtos.MerchantsController;
+using coop.Enums;
+using coop.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace coop.Controllers
 {
@@ -15,5 +29,32 @@ namespace coop.Controllers
         {
             _dbcontext = dbcontext;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyNotifications()
+        {
+            var userId = GetCurrentUserId();
+
+            var notifications = await _dbcontext.Notifications
+                .Where(n => n.UserId == userId)
+                .OrderByDescending(n => n.CreatedAt)
+                .Take(50)
+                .Select(n => new NotificationResponseDto
+                {
+                    Id = n.Id,
+                    Title = n.Title,
+                    Message = n.Message,
+                    Type = n.Type,
+                    IsRead = n.IsRead,
+                    CreatedAt = n.CreatedAt
+                })
+                .ToListAsync();
+
+            return Ok(notifications);
+        }
+
+
+        private Guid GetCurrentUserId() =>
+            Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
     }
 }
