d63ec93633f2d030ae0185234e28f8a01d692aa2
omar_alkhateeb <omaralkhateeb1w@gmail.com>
2026-08-30 17:09:16 +0300

    Add-Migration AddReviewForeignKeyColumns

---
+++ coop/Migrations/20260830140850_AddReviewForeignKeyColumns.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace coop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewForeignKeyColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
