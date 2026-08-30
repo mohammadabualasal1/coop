a2a192b5fdcf62f396e9245d5657c5389ad9d136
mohammadabualasal1 <moh2001ha@gmail.com>
2026-08-30 19:57:13 +0300

    NotificationsController

---
+++ coop/Controllers/NotificationsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly CoopDbContext _dbcontext;
+
+        public NotificationsController(CoopDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+    }
+}
