594ae1518f237545d43ac9545bb539ffda65e25c
omar_alkhateeb <omaralkhateeb1w@gmail.com>
2026-08-31 00:34:20 +0300

    AuditService

---
+++ coop/Services/IAuditService.cs
@@ -0,0 +1,7 @@
+﻿namespace coop.Services
+{
+    public interface IAuditService
+    {
+        Task LogAsync(Guid userId, string action, string entityType, Guid entityId, string? details = null);
+    }
+}
