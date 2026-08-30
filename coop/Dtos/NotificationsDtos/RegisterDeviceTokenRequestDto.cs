a5aeccd49f6179e9a4856cc915aed7a10dd4e44e
mohammadabualasal1 <moh2001ha@gmail.com>
2026-08-30 19:56:00 +0300

    RegisterDeviceTokenRequestDto

---
+++ coop/Dtos/NotificationsDtos/RegisterDeviceTokenRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using coop.Enums;
+
+namespace coop.Dtos.NotificationsDtos
+{
+    public class RegisterDeviceTokenRequestDto
+    {
+        public string Token { get; set; }
+        public DevicePlatform Platform { get; set; }
+    }
+}
